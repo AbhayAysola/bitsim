11ba6fa2628bf898129c3d00c44b02e0dc1b95b7
AbhayAysola <abhay.ayyasola@gmail.com>
2023-01-13 12:15:03 +0530

    feat(raylib-cpp)

--- bitsim.cpp
+++ bitsim.cpp
@@ -1,17 +1,21 @@
-#include "raylib.h"
+#include "raylib-cpp.hpp"
 
-int main(void) {
-  InitWindow(800, 450, "raylib [core] example - basic window");
+int main() {
+    int screenWidth = 800;
+    int screenHeight = 450;
 
-  while (!WindowShouldClose()) {
-    BeginDrawing();
-    ClearBackground(RAYWHITE);
-    DrawText("Congrats! You created your first window!", 190, 200, 20,
-             LIGHTGRAY);
-    EndDrawing();
-  }
+    raylib::Window window(screenWidth, screenHeight, "raylib-cpp - basic window");
+    SetTargetFPS(60);
 
-  CloseWindow();
+    while (!window.ShouldClose())
+    {
+        BeginDrawing();
+        window.ClearBackground(RAYWHITE);
+	
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
 
-  return 0;
+    // UnloadTexture() and CloseWindow() are called automatically.
+    return 0;
 }
